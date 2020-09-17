6a63d620c226670f95e28a110c91dc4a1f8fdc1d
blankmaru <coopskywavy@gmail.com>
2020-09-17 22:14:17 +0300

    End auth register route

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,31 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
+    // Validation Email
+    if (!validator.isEmail(req.body.email)) {
+        return res.send({ error: 'Email is not valid!' });
+    };
+
+    // Validation User
+    const userIsExist = 
+    await User.findOne({ email: req.body.email });
+    
+    if (userIsExist) {
+        return res.status(400).send({ msg: 'Email already exist' });
+    };
+
+    // Hashed Password
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+
+    // Create User
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hash
     });
 
     try {
